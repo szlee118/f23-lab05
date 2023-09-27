0ef280b9b5be60c47a0928cf622e3330d99d11c3
szlee118 <szlee118@gmail.com>
2023-09-27 07:55:24 -0400

    add inheritance

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,42 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
+
+    /**
+     * Adds an element and increments the total added count.
+     * 
+     * @param value Element to be added.
+     * @return {@code true} if the list is modified, otherwise {@code false}.
+     */
+    @Override
+    public boolean add(int value){
+        totalAdded++;
+        return super.add(value);
+    }
+
+    /**
+     * Adds all elements from the given list and updates the total added count.
+     * 
+     * @param values List of elements to be added.
+     * @return {@code true} if the list is modified, otherwise {@code false}.
+     */
+    @Override
+    public boolean addAll(IntegerList values){
+        boolean modified = false;
+        for (int i = 0; i < values.size(); i++) {
+            modified = add(values.get(i)) || modified;
+        }
+        return modified;
+    }
+
+    /**
+     * @return Total number of elements added since the list's creation.
+     */
+    public int getTotalAdded(){
+        return totalAdded;
+    }
 
 }
--- Typescript/src/InheritanceSortedIntList.ts
+++ Typescript/src/InheritanceSortedIntList.ts
@@ -1,3 +1,4 @@
+import { IntegerList } from './IntegerList.js'
 import { SortedIntList } from './hidden/SortedIntListLibrary.js'
 
 /**
@@ -10,8 +11,51 @@ import { SortedIntList } from './hidden/SortedIntListLibrary.js'
  *
  */
 
-class InheritanceSortedIntList {
-  // Write your implementation below with API documentation
+class InheritanceSortedIntList extends SortedIntList{
+    /**
+     * Constructor for InheritanceSortedIntList. Initializes the count of elements added.
+     */
+    _totalCount: number
+
+    constructor() {
+      super() // Calling the parent constructor
+      this._totalCount = 0 // underscore prefix typically denotes a private variable
+    }
+
+    /**
+     * Adds a number to the list, ensuring the list remains sorted.
+     * Increases the count of total numbers added.
+     *
+     * @param {number} number - The number to be added.
+     */
+    add(number: number): boolean {
+        this._totalCount++
+        return super.add(number); // Calls the add method from the parent class
+    }
+
+    /**
+     * Adds multiple numbers from an IntegerList to the current list, ensuring the list remains sorted.
+     * Increases the count of total numbers added by the number of elements in the input list.
+     *
+     * @param {IntegerList} numbers - An instance of IntegerList containing numbers to be added.
+     * @returns {boolean} - Returns true if at least one number was successfully added, otherwise false.
+     */
+    addAll(numbers: IntegerList): boolean {
+        let modified: boolean = false;
+        for (let i = 0; i < numbers.size(); i++) {
+            modified = this.add(numbers.get(i)) || modified;
+        }
+        return modified;
+    }
+
+    /**
+     * Retrieves the total number of elements that have been added.
+     * 
+     * @returns {number} - The total count of numbers added.
+     */
+    getTotalAdded(): number {
+        return this._totalCount;
+    }
 
 }
 
